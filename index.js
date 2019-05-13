0830386567186941b5cf0e4173e428862ade93de
Rafostar <40623528+Rafostar@users.noreply.github.com>
2019-05-13 22:09:24 +0200

    Upload version 1.0.0

---
+++ index.js
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+
+const readline = require('readline');
+const fs = require('fs');
+const homedir = require('os').homedir();
+const confDir = `${homedir}/.local/share/gnome-shell/extensions/cast-to-tv@rafostar.github.com/config`;
+
+var devices = [];
+var data = {};
+var id = 0;
+
+var myInterface = readline.createInterface({
+	input: process.stdin, output: process.stdout
+});
+
+function askQuestion(text)
+{
+	return new Promise(resolve =>
+	{
+		myInterface.question(text, (answer) => resolve(answer));
+	});
+}
+
+function saveConfig()
+{
+	var exist = fs.existsSync(confDir);
+
+	try {
+		if(!exist) fs.mkdirSync(confDir);
+		fs.writeFileSync(confDir + '/devices.json', JSON.stringify(devices, null, 1));
+		console.log('Config saved');
+	}
+	catch(err) {
+		console.log(`Error while saving config: ${err.message}`);
+	}
+
+	myInterface.close();
+}
+
+async function configure()
+{
+	var name = 'device' + id;
+	var fn = await askQuestion('Chromecast name: ');
+	var ip = await askQuestion('Chromecast IP: ');
+
+	devices[id] = { name: name, friendlyName: fn, ip: ip };
+	id++;
+
+	var answer = await askQuestion('Add another device?(y/N): ');
+	answer = answer.toLowerCase();
+
+	if(answer == 'y' || answer == 'yes') configure();
+	else saveConfig();
+}
+
+configure();
